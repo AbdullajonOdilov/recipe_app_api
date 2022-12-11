6a147c7d617b36540221102d9066f3cb3512abcf
Abdullajon <110884479+AbdullajonOdilov@users.noreply.github.com>
2022-12-11 21:05:35 +0500

    Updated

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -1,5 +1,5 @@
 """
-Tests for the Djongo admin modifications.
+Tests for the Django admin modifications.
 """
 from django.test import TestCase
 from django.contrib.auth import get_user_model
@@ -21,13 +21,20 @@ class AdminSiteTests(TestCase):
         self.user = get_user_model().objects.create_user(
             email='user@example.com',
             password='testpass123',
-            name='Test User',
+            name='Test User'
         )
 
-    def test_users_list(self):
+    def test_users_lists(self):
         """Test that users are listed on page."""
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        """Test the edit user page works."""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
